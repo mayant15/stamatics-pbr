32a623002f903d74f4924010a8b634f5b83d4659
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-06-09 17:25:31 +0530

    Finish week-7

--- src/scene.cpp
+++ src/scene.cpp
@@ -13,67 +13,66 @@
 
 Scene PBR_SCENE_RTWEEKEND = {
     // Red ball
-    Actor {
+    Actor{
         std::make_shared<Material>(
-            Colorf { 1.0, 0.1, 0.1 }, // Color
-            Colorf { 0.0, 0.0, 0.0 },  // Emission
-            new path::DiffuseBRDF
-        ),
-        SphereGeometry {
-            Vec { 1.5, 1.0, 0.0 },   // Position
-            1.0                      // Radius
-        }
-    },
+            Colorf{1.0, 0.1, 0.1}, // Color
+            Colorf{0.0, 0.0, 0.0}, // Emission
+            new path::DiffuseBRDF),
+        SphereGeometry{
+            Vec{3.0, 1.0, 0.0}, // Position
+            1.0                 // Radius
+        }},
+    // blue ball
+    Actor{
+        std::make_shared<Material>(
+            Colorf{0.1, 0.1, 1.0}, // Color
+            Colorf{0.0, 0.0, 0.0}, // Emission
+            new path::DiffuseBRDF),
+        SphereGeometry{
+            Vec{-3.0, 1.0, 0.0}, // Position
+            1.0                  // Radius
+        }},
 
     // Light 1
-    Actor {
+    Actor{
         std::make_shared<Material>(
-            Colorf { 1.0, 1.0, 1.0 }, // Color
-            Colorf { 6.0, 6.0, 6.0 },  // Emission
-            new path::DiffuseBRDF
-        ),
-        SphereGeometry {
-            Vec { 6.0, 4.5, -4.0 },   // Position
-            3.0                      // Radius
-        }
-    },
+            Colorf{1.0, 1.0, 1.0}, // Color
+            Colorf{6.0, 6.0, 6.0}, // Emission
+            new path::DiffuseBRDF),
+        SphereGeometry{
+            Vec{6.0, 4.5, -4.0}, // Position
+            3.0                  // Radius
+        }},
 
     // Light 2
-    Actor {
+    Actor{
         std::make_shared<Material>(
-            Colorf { 1.0, 1.0, 1.0 }, // Color
-            Colorf { 6.0, 6.0, 6.0 },  // Emission
-            new path::DiffuseBRDF
-        ),
-        SphereGeometry {
-            Vec { -6.0, 4.5, -4.0 },   // Position
-            3.0                      // Radius
-        }
-    },
+            Colorf{1.0, 1.0, 1.0}, // Color
+            Colorf{6.0, 6.0, 6.0}, // Emission
+            new path::DiffuseBRDF),
+        SphereGeometry{
+            Vec{-6.0, 4.5, -4.0}, // Position
+            3.0                   // Radius
+        }},
 
     // Mirror
-    Actor {
+    Actor{
         std::make_shared<Material>(
-            Colorf { 1.0, 1.0, 1.0 }, // Color
-            Colorf { 0.0, 0.0, 0.0 },  // Emission
-            new path::SpecularBRDF
-        ),
-        SphereGeometry {
-            Vec { -1.5, 1.0, 0.0 },   // Position
-            1.0                      // Radius
-        }
-    },
+            Colorf{1.0, 1.0, 1.0}, // Color
+            Colorf{0.0, 0.0, 0.0}, // Emission
+            new path::SpecularBRDF),
+        SphereGeometry{
+            Vec{0.0, 1.0, 0.0}, // Position
+            1.0                 // Radius
+        }},
 
     // Floor
-    Actor {
+    Actor{
         std::make_shared<Material>(
-            Colorf { 0.1, 1.0, 0.1 }, // Color
-            Colorf { 0.0, 0.0, 0.0 },  // Emission
-            new path::DiffuseBRDF
-        ),
-        SphereGeometry {
-            Vec { 0.0, -1e5, 0.0 },  // Position
-            1e5                      // Radius
-        }
-    }
-};
+            Colorf{0.1, 1.0, 0.1}, // Color
+            Colorf{0.0, 0.0, 0.0}, // Emission
+            new path::DiffuseBRDF),
+        SphereGeometry{
+            Vec{0.0, -1e5, 0.0}, // Position
+            1e5                  // Radius
+        }}};
